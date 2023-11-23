16934d6fa5d3f3842811e04f36674dbf8b3c528a
abhishekb0 <142288733+abhishekb0@users.noreply.github.com>
2023-11-23 09:59:27 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        primary: '#F5385D',
+      }
+    },
+  },
+  plugins: [],
+}
+
